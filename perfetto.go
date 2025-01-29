59fea63080dd172ebdfee9f94e28650109562815
Alberto Donizetti <alb.donizetti@gmail.com>
2025-01-29 17:36:53 +0100

    initial commit

---
+++ perfetto.go
@@ -0,0 +1,188 @@
+package perfetto
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"os"
+
+	"google.golang.org/protobuf/proto"
+
+	pp "github.com/ALTree/perfetto/proto"
+)
+
+type Trace struct {
+	pt  pp.Trace
+	TID uint32
+}
+
+type Process struct {
+	Pid  int32
+	Name string
+	Uuid uint64
+}
+
+func NewProcess(pid int32, name string) Process {
+	return Process{
+		Pid:  pid,
+		Name: name,
+		Uuid: rand.Uint64(),
+	}
+}
+
+func (p Process) Emit() *pp.TracePacket_TrackDescriptor {
+	return &pp.TracePacket_TrackDescriptor{
+		&pp.TrackDescriptor{
+			Uuid: &p.Uuid,
+			Process: &pp.ProcessDescriptor{
+				Pid:         &p.Pid,
+				ProcessName: &p.Name,
+			}},
+	}
+}
+
+type Thread struct {
+	Pid, Tid int32
+	Name     string
+	Uuid     uint64
+}
+
+func NewThread(pid, tid int32, name string) Thread {
+	return Thread{
+		Pid:  pid,
+		Tid:  tid,
+		Name: name,
+		Uuid: rand.Uint64(),
+	}
+}
+
+func (t Thread) Emit() *pp.TracePacket_TrackDescriptor {
+	return &pp.TracePacket_TrackDescriptor{
+		&pp.TrackDescriptor{
+			Uuid: &t.Uuid,
+			Thread: &pp.ThreadDescriptor{
+				Pid:        &t.Pid,
+				Tid:        &t.Tid,
+				ThreadName: &t.Name,
+			}},
+	}
+}
+
+func (t *Trace) AddProcess(pid int32, name string) Process {
+	pr := NewProcess(pid, name)
+	//t.procs = append(t.procs, pr)
+	t.pt.Packet = append(t.pt.Packet, &pp.TracePacket{Data: pr.Emit()})
+	return pr
+}
+
+func (t *Trace) AddThread(pid, tid int32, name string) Thread {
+	tr := NewThread(pid, tid, name)
+	//t.threads = append(t.threads, tr)
+	t.pt.Packet = append(t.pt.Packet, &pp.TracePacket{Data: tr.Emit()})
+	return tr
+}
+
+type Event struct {
+	Timestamp uint64
+	Type      pp.TrackEvent_Type
+	Name      string
+	TrackUuid uint64
+}
+
+var EventSliceBegin pp.TrackEvent_Type = pp.TrackEvent_TYPE_SLICE_BEGIN
+var EventSliceEnd pp.TrackEvent_Type = pp.TrackEvent_TYPE_SLICE_END
+
+func (p Process) NewEvent(Type pp.TrackEvent_Type, ts uint64, name string) Event {
+	return Event{
+		Timestamp: ts,
+		Type:      Type,
+		Name:      name,
+		TrackUuid: p.Uuid,
+	}
+}
+
+func (p Process) InstantEvent(ts uint64, name string) Event {
+	return p.NewEvent(pp.TrackEvent_TYPE_INSTANT, ts, name)
+}
+
+func (p Process) StartSlice(ts uint64, name string) Event {
+	return p.NewEvent(pp.TrackEvent_TYPE_SLICE_BEGIN, ts, name)
+}
+
+func (p Process) EndSlice(ts uint64) Event {
+	return p.NewEvent(pp.TrackEvent_TYPE_SLICE_END, ts, "")
+}
+
+func (t Thread) NewEvent(Type pp.TrackEvent_Type, ts uint64, name string) Event {
+	return Event{
+		Timestamp: ts,
+		Type:      Type,
+		Name:      name,
+		TrackUuid: t.Uuid,
+	}
+}
+
+func (t Thread) InstantEvent(ts uint64, name string) Event {
+	return t.NewEvent(pp.TrackEvent_TYPE_INSTANT, ts, name)
+}
+
+func (t Thread) StartSlice(ts uint64, name string) Event {
+	return t.NewEvent(pp.TrackEvent_TYPE_SLICE_BEGIN, ts, name)
+}
+
+func (t Thread) EndSlice(ts uint64) Event {
+	return t.NewEvent(pp.TrackEvent_TYPE_SLICE_END, ts, "")
+}
+
+func (e Event) Emit() *pp.TracePacket_TrackEvent {
+	te := &pp.TracePacket_TrackEvent{
+		&pp.TrackEvent{
+			TrackUuid: &e.TrackUuid,
+			Type:      &e.Type,
+		},
+	}
+
+	if name := e.Name; name != "" {
+		te.TrackEvent.NameField = &pp.TrackEvent_Name{name}
+	}
+
+	return te
+}
+
+func (t *Trace) AddEvent(e Event) {
+	t.pt.Packet = append(t.pt.Packet,
+		&pp.TracePacket{
+			Timestamp:                       &e.Timestamp,
+			Data:                            e.Emit(),
+			OptionalTrustedPacketSequenceId: &pp.TracePacket_TrustedPacketSequenceId{t.TID},
+		})
+}
+
+func Test() {
+	trace := Trace{TID: 32}
+	trace.AddProcess(1, "my process")
+	t1 := trace.AddThread(1, 2, "thread1")
+	t2 := trace.AddThread(1, 3, "thread2")
+
+	trace.AddEvent(t1.StartSlice(200, "download"))
+	trace.AddEvent(t1.InstantEvent(400, "event1"))
+	trace.AddEvent(t1.StartSlice(500, "process"))
+	trace.AddEvent(t1.EndSlice(800))
+	trace.AddEvent(t1.EndSlice(1000))
+
+	trace.AddEvent(t2.StartSlice(300, "slice2"))
+	trace.AddEvent(t2.InstantEvent(500, "event2"))
+	trace.AddEvent(t2.EndSlice(800))
+
+	data, err := proto.Marshal(&trace.pt)
+	if err != nil {
+		panic(err)
+	}
+
+	var rtt pp.Trace
+	err = proto.Unmarshal(data, &rtt)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", rtt.Packet[4].GetTrackEvent().GetName())
+	os.WriteFile("test.bin", data, 0666)
+}
---
+++ perfetto_test.go
@@ -0,0 +1,204 @@
+package perfetto
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	pp "github.com/ALTree/perfetto/proto"
+	"google.golang.org/protobuf/proto"
+)
+
+func RoundTrip(t *testing.T, trace Trace) pp.Trace {
+	data, err := proto.Marshal(&trace.pt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var rtt pp.Trace
+	err = proto.Unmarshal(data, &rtt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return rtt
+}
+
+func ProcessName(p *pp.TracePacket) string {
+	return p.GetTrackDescriptor().GetProcess().GetProcessName()
+}
+
+func ProcessPid(p *pp.TracePacket) int32 {
+	return p.GetTrackDescriptor().GetProcess().GetPid()
+}
+
+func ThreadName(p *pp.TracePacket) string {
+	return p.GetTrackDescriptor().GetThread().GetThreadName()
+}
+
+func ThreadPid(p *pp.TracePacket) int32 {
+	return p.GetTrackDescriptor().GetThread().GetPid()
+}
+
+func ThreadTid(p *pp.TracePacket) int32 {
+	return p.GetTrackDescriptor().GetThread().GetTid()
+}
+
+func EventTimestamp(p *pp.TracePacket) uint64 {
+	return p.GetTimestamp()
+}
+
+func EventName(p *pp.TracePacket) string {
+	return p.GetTrackEvent().GetName()
+}
+
+func EventType(p *pp.TracePacket) string {
+	return p.GetTrackEvent().GetType().String()
+}
+
+func EventTrackUuid(p *pp.TracePacket) uint64 {
+	return p.GetTrackEvent().GetTrackUuid()
+}
+
+func TestAddProcess(t *testing.T) {
+	trace := Trace{TID: 32}
+	trace.AddProcess(1, "process #6")
+	tr := RoundTrip(t, trace)
+
+	AssertEq("trace length", t, len(tr.Packet), 1)
+	AssertEq("Name", t, ProcessName(tr.Packet[0]), "process #6")
+	AssertEq("Pid", t, ProcessPid(tr.Packet[0]), 1)
+}
+
+func TestAddThread(t *testing.T) {
+	trace := Trace{TID: 32}
+	trace.AddProcess(1, "process #6")
+	trace.AddThread(1, 2, "thread #1")
+	trace.AddThread(1, 3, "thread #2")
+	tr := RoundTrip(t, trace)
+
+	AssertEq("trace length", t, len(tr.Packet), 3)
+
+	t1, t2 := tr.Packet[1], tr.Packet[2]
+	AssertEq("Thread #1 Name", t, ThreadName(t1), "thread #1")
+	AssertEq("Thread #1 Tid", t, ThreadTid(t1), 2)
+	AssertEq("Thread #1 Pid", t, ThreadPid(t1), 1)
+	AssertEq("Thread #2 Name", t, ThreadName(t2), "thread #2")
+	AssertEq("Thread #2 Tid", t, ThreadTid(t2), 3)
+	AssertEq("Thread #2 Pid", t, ThreadPid(t2), 1)
+}
+
+func TestAddManyThreads(t *testing.T) {
+	trace := Trace{TID: 32}
+	trace.AddProcess(1, "process #3")
+	for i := range 100 {
+		trace.AddThread(1, int32(i), fmt.Sprintf("Thread #%v", i))
+	}
+
+	tr := RoundTrip(t, trace)
+	AssertEq("trace length", t, len(tr.Packet), 101)
+	for i := range int32(100) {
+		tr := tr.Packet[i+1]
+		AssertEq("Thread Name", t, ThreadName(tr), fmt.Sprintf("Thread #%v", i))
+		AssertEq("Thread Tid", t, ThreadTid(tr), i)
+		AssertEq("Thread Pid", t, ThreadPid(tr), 1)
+	}
+}
+
+func TestInstantEvent(t *testing.T) {
+	trace := Trace{TID: 32}
+	trace.AddProcess(1, "process #1")
+	thr := trace.AddThread(1, 2, "Thread #1")
+
+	trace.AddEvent(thr.InstantEvent(500, "Event #1"))
+
+	tr := RoundTrip(t, trace)
+	AssertEq("trace length", t, len(tr.Packet), 3)
+	p := tr.Packet[2]
+	AssertEq("Timestamp", t, EventTimestamp(p), 500)
+	AssertEq("Name", t, EventName(p), "Event #1")
+	AssertEq("Type", t, EventType(p), "TYPE_INSTANT")
+	AssertEq("Track UUID", t, EventTrackUuid(p), thr.Uuid)
+
+}
+
+func TestSliceEvent(t *testing.T) {
+	trace := Trace{TID: 32}
+	trace.AddProcess(1, "process #1")
+	thr := trace.AddThread(1, 2, "Thread #1")
+
+	trace.AddEvent(thr.StartSlice(1000, "Slice #1"))
+	trace.AddEvent(thr.EndSlice(1500))
+
+	tr := RoundTrip(t, trace)
+	AssertEq("trace length", t, len(tr.Packet), 4)
+	p1, p2 := tr.Packet[2], tr.Packet[3]
+	AssertEq("start Timestamp", t, EventTimestamp(p1), 1000)
+	AssertEq("start Name", t, EventName(p1), "Slice #1")
+	AssertEq("start Type", t, EventType(p1), "TYPE_SLICE_BEGIN")
+	AssertEq("start Track UUID", t, EventTrackUuid(p1), thr.Uuid)
+	AssertEq("end Timestamp", t, EventTimestamp(p2), 1500)
+	AssertEq("end Name", t, EventName(p2), "")
+	AssertEq("end Type", t, EventType(p2), "TYPE_SLICE_END")
+	AssertEq("end Track UUID", t, EventTrackUuid(p2), thr.Uuid)
+}
+
+func TestManyEvents(t *testing.T) {
+	trace := Trace{TID: 32}
+	trace.AddProcess(1, "process #1")
+	thr1 := trace.AddThread(1, 2, "Thread #1")
+	thr2 := trace.AddThread(1, 3, "Thread #2")
+
+	for i := range uint64(100) {
+		if i%2 == 0 {
+			trace.AddEvent(thr1.StartSlice(i*100, "thr1 func"))
+			trace.AddEvent(thr1.EndSlice(i*100 + 50))
+		} else {
+			trace.AddEvent(thr2.StartSlice(i*100, "thr2 func"))
+			trace.AddEvent(thr2.EndSlice(i*100 + 50))
+		}
+	}
+
+	tr := RoundTrip(t, trace)
+	AssertEq("trace length", t, len(tr.Packet), 3+2*100)
+	data, _ := proto.Marshal(&trace.pt)
+	os.WriteFile("test.bin", data, 0666)
+
+	packets := tr.Packet[3:]
+	for i := range uint64(200) {
+		p := packets[i]
+		ts, name, typ, uuid := func() (uint64, string, string, uint64) {
+			j := i / 2
+			switch i % 4 {
+			case 0:
+				return j * 100, "thr1 func", "TYPE_SLICE_BEGIN", thr1.Uuid
+			case 1:
+				return j*100 + 50, "", "TYPE_SLICE_END", thr1.Uuid
+			case 2:
+				return j * 100, "thr2 func", "TYPE_SLICE_BEGIN", thr2.Uuid
+			default:
+				return j*100 + 50, "", "TYPE_SLICE_END", thr2.Uuid
+			}
+		}()
+		AssertEq("Timestamp", t, EventTimestamp(p), ts)
+		AssertEq("Name", t, EventName(p), name)
+		AssertEq("Type", t, EventType(p), typ)
+		AssertEq("Track UUID", t, EventTrackUuid(p), uuid)
+	}
+}
+
+// ---- { testing helpers } --------------------------------
+
+func AssertEq[V comparable](fmt string, t *testing.T, got, exp V) {
+	t.Helper()
+	if exp != got {
+		t.Errorf("For %s\ngot %v\nexp %v", fmt, got, exp)
+	}
+}
+
+func AssertNeq[V comparable](fmt string, t *testing.T, got, exp V) {
+	t.Helper()
+	if exp == got {
+		t.Errorf("For %s\ngot %v\nexp != %v", fmt, got, exp)
+	}
+}
